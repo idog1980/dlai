0a5ddb7ce60e6069b97da07ac4dee82c92c430bc
MrIdoGold <ido@goldbergs.co.il>
2023-10-22 16:14:30 +0300

    removing structure

--- structure.ps1
+++ structure.ps1
@@ -1,11 +1,16 @@
 New-Item -ItemType Directory -Name "my-repo"
 Set-Location "my-repo"
 
-$companies = @("DataloopAI", "IdoAI", "MichaelAI")
+$companies = @("dataloopAi", "idoAI", "michaelAI")
 $environments = @("development", "staging", "production")
 $cloudProviders = @("aws", "gcp")
+# Create the common directory
+New-Item -ItemType Directory -Path "common"
+New-Item -ItemType Directory -Path "common\modules"
+New-Item -ItemType Directory -Path "common\helm"
+New-Item -ItemType Directory -Path "common\templates"
+New-Item -ItemType File -Path "common\README.md"
 
-# Loop through companies and environments
 foreach ($company in $companies) {
     foreach ($environment in $environments) {
         $envPath = Join-Path -Path $company -ChildPath "environment\$environment"
@@ -14,16 +19,14 @@ foreach ($company in $companies) {
             $providerPath = Join-Path -Path $envPath -ChildPath $provider
             New-Item -ItemType Directory -Path $providerPath
             New-Item -ItemType Directory -Path (Join-Path -Path $providerPath -ChildPath "terraform")
+
+            # Add a "1.txt" file to the current directory
+            New-Item -ItemType File -Path $providerPath -Name "1.txt"
         }
     }
 }
 
-New-Item -ItemType Directory -Path "common"
-New-Item -ItemType Directory -Path "common\modules"
-New-Item -ItemType Directory -Path "common\scripts"
-New-Item -ItemType Directory -Path "common\templates"
-New-Item -ItemType File -Path "common\README.md"
-
+# Create a README file for the main repository
 New-Item -ItemType File -Name "README.md"
 
 Write-Host "Folder structure deployed successfully!"
