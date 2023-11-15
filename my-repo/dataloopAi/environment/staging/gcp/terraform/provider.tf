c6f9bb348cdc3834e437902c1a43399691ba3071
MrIdoGold <ido@goldbergs.co.il>
2023-11-15 19:22:44 +0200

    remving helm

--- my-repo/dataloopAi/environment/staging/gcp/terraform/provider.tf
+++ my-repo/dataloopAi/environment/staging/gcp/terraform/provider.tf
@@ -24,16 +24,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.21.1"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.10.1"
-    }
-  }
-}
-provider "helm" {
-  kubernetes {
-    config_path    = "~/.kube/config"
-    config_context = var.cluster_name
   }
 }
 
