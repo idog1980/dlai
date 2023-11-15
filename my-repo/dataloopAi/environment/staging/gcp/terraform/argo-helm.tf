0bf33701c4beffba05d847643f789b96e74345f3
MrIdoGold <ido@goldbergs.co.il>
2023-11-15 13:19:56 +0200

    adding time wait

--- my-repo/dataloopAi/environment/staging/gcp/terraform/argo-helm.tf
+++ my-repo/dataloopAi/environment/staging/gcp/terraform/argo-helm.tf
@@ -1,8 +1,14 @@
+resource "time_sleep" "wait_5_min" {
+  create_duration = "5m"
+}
+
+
 module "argocd" {
   source   = "aigisuk/argocd/kubernetes"
   insecure = true
 
   depends_on = [
-    module.gke
+    module.gke,
+    time_sleep.wait_5_min
   ]
 }
