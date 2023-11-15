2705a91f5839a96caab46da85da655f43caba92f
MrIdoGold <ido@goldbergs.co.il>
2023-11-15 11:08:52 +0200

    adding monitoring

---
+++ my-repo/dataloopAi/environment/staging/gcp/terraform/monitoring-deployment.tf
@@ -0,0 +1,28 @@
+resource "kubernetes_namespace" "monitoring-namespace" {
+  metadata {
+    name = "monitoring"
+  }
+  depends_on = [ module.gke ]
+}
+
+resource "helm_release" "prometheus" {
+  name = "prometheus"
+  namespace = "monitoring"
+  chart = "/apps/prometheus"
+  set {
+    name = "service.type"
+    value = "NodePort"
+  }
+  depends_on = [ kubernetes_namespace.monitoring-namespace ]
+}
+
+resource "helm_release" "grafana" {
+  name = "grafana"
+  namespace = "monitoring"
+  chart = "/apps/grafana"
+  set {
+    name = "service.type"
+    value = "NodePort"
+  }
+  depends_on = [ kubernetes_namespace.monitoring-namespace ]
+}
