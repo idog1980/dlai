83aaef7ec761a424406737f618e13f2761ec58b2
MrIdoGold <ido@goldbergs.co.il>
2023-11-15 11:46:50 +0200

    switching prom-stackk

--- my-repo/dataloopAi/environment/staging/gcp/terraform/monitoring-deployment.tf
+++ my-repo/dataloopAi/environment/staging/gcp/terraform/monitoring-deployment.tf
@@ -1,28 +1,32 @@
-resource "kubernetes_namespace" "monitoring-namespace" {
-  metadata {
-    name = "monitoring"
+resource "kubernetes_manifest" "grafana_application" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "grafana"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL  = "https://github.com/idog1980/dlai.git"
+        path     = "my-repo/dataloopAi/environment/staging/gcp/apps/grafana"
+        targetRevision = "HEAD"
+        chart          = "grafana"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "monitoring"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = [
+          "CreateNamespace=true"
+        ]
+      }
+    }
   }
-  depends_on = [module.gke]
-}
-
-resource "helm_release" "prometheus" {
-  name      = "prometheus"
-  namespace = "monitoring"
-  chart     = "../apps/prometheus"
-  set {
-    name  = "service.type"
-    value = "NodePort"
-  }
-  depends_on = [kubernetes_namespace.monitoring-namespace]
-}
-
-resource "helm_release" "grafana" {
-  name      = "grafana"
-  namespace = "monitoring"
-  chart     = "../apps/grafana"
-  set {
-    name  = "service.type"
-    value = "NodePort"
-  }
-  depends_on = [kubernetes_namespace.monitoring-namespace]
 }
